110ae905bb7aa6c4d2177ed2e53d24d22e668aaf
SaveliyKarpushin <113942678+SaveliyKarpushin@users.noreply.github.com>
2022-10-14 12:38:35 +0300

    бета 1
    1

---
+++ LAB-3 Saveliy/Product.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_3_Saveliy
+{
+    class Product
+    {
+        private string имя;
+        public string Имя
+            {
+            get
+            {
+                return имя;
+            }
+            set
+            {
+                имя = value;
+            }
+            }
+
+        private string произв;
+        public string Произв
+        {
+            get
+            {
+                return произв;
+            }
+            set
+            {
+                произв = value;
+            }
+        }
+        private double цена;
+        public double Цена
+        {
+            get
+            {
+                return цена;
+            }
+            set
+            {
+                цена = value;
+            }
+        }
+        private double срок;
+        public double Срок
+        {
+            get
+            {
+                return срок;
+            }
+            set
+            {
+                срок = value;
+            }
+        }
+        private int кол;
+        public int Кол
+        {
+            get
+            {
+                return кол;
+            }
+            set
+            {
+                кол = value;
+            }
+        }
+
+
+
+    }
+}
